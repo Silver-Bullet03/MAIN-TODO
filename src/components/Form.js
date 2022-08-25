d68b643e80f1b6487a394886373cf8c89652bf53
Silver-Bullet03 <venkatram.sirasala@gmail.com>
2022-08-25 13:29:52 -0400

    3 files modified

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,27 +1,41 @@
-import React , {useState} from 'react'
+import React, { useState } from 'react';
 
-export default function Form(props){
+export default function Form(props) {
+  const [todo, setTodo] = useState('');
 
-  const [todo ,setTodo] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  const handleSubmit = e => {
-     e.preventDefault();
-    
     props.onSubmit(todo);
     setTodo('');
-  }
+  };
 
-  const handleChange = e => {
+  const handleChange = (e) => {
     setTodo(e.target.value);
-  }
+  };
+
+  const handleDelete = (e) => {
+    //e.preventDefault();
+    props.onClick();
+    setTodo('');
+  };
 
   return (
-  
-    <div className = 'form-cont'>
-         <form className = 'form' onSubmit = {handleSubmit}>
-             <input type = 'text' placeholder = 'type here' onChange={handleChange} value = {todo}/>
-             <button className = 'todo-button' type = 'submit'>ADD </button>
-         </form>
+    <div className="form-cont">
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="type here"
+          onChange={handleChange}
+          value={todo}
+        />
+        <button className="todo-button" type="submit">
+          ADD{' '}
+        </button>
+      </form>
+      <button onClick={handleDelete} className="delete-button">
+        DELETE ALL
+      </button>
     </div>
-  )
+  );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,32 +1,36 @@
-import React, {useState} from 'react'
-import Form from './Form'
-import List from './List'
+import React, { useState } from 'react';
+import Form from './Form';
+import List from './List';
 
-export default function Main(){
-  
-  const [todos , setTodos] = useState([]);
-  
-  const addTodo = todo => {
+export default function Main() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
     if (!todo || /^\s*$/.test(todo)) {
       return;
     }
-    setTodos([...todos,todo]);
-  }
+    setTodos([...todos, todo]);
+  };
 
-  const deleteTodo = todo => {
-    setTodos([...todos].filter(ele => ele !== todo));
-  }
+  const deleteTodo = (todo) => {
+    setTodos([...todos].filter((ele) => ele !== todo));
+  };
 
-  const editTodo = (todo , newTodo) => {
-    if(!newTodo || /^\s*$/.test(newTodo)) {
+  const editTodo = (todo, newTodo) => {
+    if (!newTodo || /^\s*$/.test(newTodo)) {
       return;
     }
-    setTodos([...todos].map( ele => (ele === todo ? newTodo : ele)));
-  }
-  return(
+    setTodos([...todos].map((ele) => (ele === todo ? newTodo : ele)));
+  };
+
+  const deleteAll = () => {
+    setTodos([]);
+  };
+
+  return (
     <div>
-       <Form onSubmit = {addTodo} />
-       <List data = {todos} deleteTodo = {deleteTodo} editTodo = {editTodo}/>
+      <Form onSubmit={addTodo} onClick={deleteAll} />
+      <List data={todos} deleteTodo={deleteTodo} editTodo={editTodo} />
     </div>
-  )
+  );
 }
